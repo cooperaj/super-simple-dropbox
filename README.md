5ebf0d940a4a9944511468993bb79d9286df4e58
Adam Cooper <adam@acpr.dev>
2020-03-18 16:10:30 +0000

    Rejig env vars and get it ready for release

---
+++ README.md
@@ -0,0 +1,49 @@
+Dropbox
+=======
+
+This provides a multipart upload capability to S3 or an S3 compatible service. Tested to work with Minio.
+
+## Prerequisites for Minio
+ * A configured Minio instance with a user that *isn't* the root account - see Minio's multi-user [documentation](https://docs.min.io/docs/minio-multi-user-quickstart-guide.html)
+ * The Minio Client (mc) configured to connect to that instance.
+
+You need to add the correct S3 permission policy to the user this service will connect with. The included [policy file](putonly.json) assumes a bucket name of `uploads` and will need modification if this is not correct for you.
+
+```bash
+# assumes the configured Minio instance is named 'minio'
+# replace <USERNAME> with your non-root username
+
+$ mc admin policy add minio putonly putonly.json
+$ mc admin policy set minio putonly user=<USERNAME>
+```
+
+## Running
+
+Ensure the necessary environment variables are configured.
+
+| Env. variable | Required | Default | Notes |
+| ---- | ---- | ---- | ---- |
+| S3_KEY | Yes | | The 'username' |
+| S3_SECRET | Yes | | The 'password' |
+| S3_ENDPOINT | No | Whatever AWS uses by default | The endpoint to connect to, this will be the URI of your Minio instance e.g. `https://storage.example.com` |
+| S3_BUCKET | No | 'uploads' | The bucket to upload to - the configured user must have the correct policy permissions |
+| PORT | No | 8080 | The port this service listens on |
+| SESSION_SECRET | No | A random value | The session hashing salt value. Randomly chosen unless specified |
+| UPPY_SECRET | No | A random value | The Uppy hashing salt value. Randomly chosen unless specified |
+
+```bash
+# Will start the service listening on 0.0.0.0:${PORT}
+
+$ npm run start
+```
+
+## Running on Dokku
+
+ * Create the application
+ * Attach the Dokku repository as a remote
+ * Configure configuration settings  
+   e.g. `dokku config:set --no-restart S3_ENDPOINT=https://storage.example.com`  
+ * Push this repo to the Dokku remote
+ 
+## Important
+This application utilises a session that uses secure only cookies. This means you will need to have a functioning SSL setup - with Dokku just use the LetsEncrypt plugin. Simples.
--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var session = require('express-session')
-var companion = require('@uppy/companion')
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const companion = require('@uppy/companion')
+const str = require('@supercharge/strings')
 
-var app = express()
+if (typeof process.env.S3_KEY == 'undefined' 
+        || typeof process.env.S3_SECRET == 'undefined') {
+
+    console.log('ERROR environment variables S3_KEY, S3_SECRET are required.')
+    process.exit(1)        
+}
+
+const port = process.env.PORT || 8080
+const options = {
+    providerOptions: {
+        s3: {
+            getKey: (req, filename, metadata) => filename,
+            key: process.env.S3_KEY,
+            secret: process.env.S3_SECRET || 'uploads',
+            bucket: process.env.S3_BUCKET,
+            acl: 'private',
+            awsClientOptions: {
+                s3ForcePathStyle: true
+            }
+        }
+    },
+    server: {
+        host: `localhost:${port}`,
+        protocol: 'http'
+    },
+    filePath: '/tmp',
+    secret: process.env.UPPY_SECRET || str.random(32),
+    debug: true
+}
+
+Object.assign(options.providerOptions.s3.awsClientOptions, process.env.S3_ENDPOINT && { endpoint: process.env.S3_ENDPOINT })
+
+let app = express()
 app.use(bodyParser.json())
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || str.random(32),
     resave: true,
     saveUninitialized: true,
     cookie: {
@@ -31,29 +64,6 @@ app.use((req, res, next) => {
 
     next()
 })
-
-const options = {
-    providerOptions: {
-        s3: {
-            getKey: (req, filename, metadata) => filename,
-            key: process.env.MINIO_KEY,
-            secret: process.env.MINIO_SECRET,
-            bucket: process.env.BUCKET,
-            acl: 'private',
-            awsClientOptions: {
-                endpoint: process.env.MINIO_HOST,
-                s3ForcePathStyle: true
-            }
-        }
-    },
-    server: {
-        host: 'localhost:8080',
-        protocol: 'http'
-    },
-    filePath: '/tmp',
-    secret: process.env.MINIO_HASH_SECRET,
-    debug: true
-}
   
 app.use(companion.app(options))
 
@@ -67,4 +77,6 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500).json({ message: err.message, error: err })
 })
 
-companion.socket(app.listen(8080), options)
+companion.socket(app.listen(port), options)
+
+console.log(`Server running on port ${port}`)
